891785000fb917fe0cb0f5f2642b316de3db557d
randrei12 <rantaandrei12@gmail.com>
2024-09-10 21:11:18 +0300

    v3 (12th class edition)

--- public/dates.js
+++ public/dates.js
@@ -1,4 +1,4 @@
 export default {
-    "CALVAR_START_DATE": "September 11 2023",
-    "CALVAR_END_DATE": "June 21 2024"
+    "CALVAR_START_DATE": "September 9 2024",
+    "CALVAR_END_DATE": "June 6 2025"
 }
